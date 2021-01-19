c97bf3784e95e1e191180899dd5647362fc1585c
fancky2019 <709737588@qq.com>
2021-01-19 13:38:24 +0800

    学生列表

--- ordermigratedbtool/src/main/java/com/onlyedu/ordermigratedbtool/model/dto/UserInfoDto.java
+++ ordermigratedbtool/src/main/java/com/onlyedu/ordermigratedbtool/model/dto/UserInfoDto.java
@@ -1,8 +1,10 @@
 package com.onlyedu.ordermigratedbtool.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.onlyedu.ordermigratedbtool.model.pojo.Page;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,6 +21,8 @@ public class UserInfoDto extends Page {
     private String mobilePhone;
     private String tel;
     private String school;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime regTime;
     private Boolean relativeState;
     private String eOSUserInfo;
